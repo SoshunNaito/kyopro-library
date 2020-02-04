6ef8a4798e76f1a4824e02bb481ee3c5b75d0728
SoshunNaito <50867811+SoshunNaito@users.noreply.github.com>
2020-02-05 03:05:01 +0900

    セグ木を整備しました

--- segtree.cpp
+++ segtree.cpp
@@ -1,10 +1,98 @@
 /*
 ///////////////   セグメント木
-const ll INF = (ll)1234567891011121314;
 
-ll segtree_func(ll a, ll b) {// 最小値を得るセグメント木ができる。
+// ここでセグ木の設定をする
+
+// #define SEGTREE_DATA_ADD			// 和をとるセグ木
+#define SEGTREE_DATA_MAX			// 最大値をとるセグ木
+// #define SEGTREE_DATA_MIN			// 最小値をとるセグ木
+
+// #define SEGTREE_LAZY_SET			// 区間代入する遅延セグ木
+#define SEGTREE_LAZY_ADD			// 区間加算する遅延セグ木
+// #define SEGTREE_LAZY_MAX			// 最大値を更新する遅延セグ木
+// #define SEGTREE_LAZY_MIN			// 最小値を更新する遅延セグ木
+
+
+const ll INF = 1LL << 60;
+#ifdef SEGTREE_DATA_ADD
+const ll segtree_data_init = 0;
+inline ll segtree_data_merge(ll a, ll b) {// 要素aのbの和
+	return a + b;
+}
+#endif
+#ifdef SEGTREE_DATA_MAX
+const ll segtree_data_init = -INF;
+inline ll segtree_data_merge(ll a, ll b) {// 要素aのbの最大値
+	return max(a, b);
+}
+#endif
+#ifdef SEGTREE_DATA_MIN
+const ll segtree_data_init = INF;
+inline ll segtree_data_merge(ll a, ll b) {// 要素aのbの最小値
 	return min(a, b);
 }
+#endif
+
+#ifdef SEGTREE_LAZY_SET
+const ll segtree_lazy_init = 0;
+inline ll segtree_lazy_merge(ll a, ll b) {// 演算子aとbのマージ
+	return b;
+}
+inline ll segtree_lazy_multi(ll a, ll n) {// 演算子aをn個つなげたもの
+	return a;
+}
+inline ll segtree_lazy_divide(ll a) {// 演算子aを二個に分ける
+	return a;
+}
+inline ll segtree_lazy_apply(ll a, ll x) {// aに演算子xを適用する
+	return x;
+}
+#endif
+#ifdef SEGTREE_LAZY_ADD
+const ll segtree_lazy_init = 0;
+inline ll segtree_lazy_merge(ll a, ll b) {// 演算子aとbのマージ
+	return a + b;
+}
+inline ll segtree_lazy_multi(ll a, ll n) {// 演算子aをn個つなげたもの
+	return a * n;
+}
+inline ll segtree_lazy_divide(ll a) {// 演算子aを二個に分ける
+	return a / 2;
+}
+inline ll segtree_lazy_apply(ll a, ll x) {// aに演算子xを適用する
+	return a + x;
+}
+#endif
+#ifdef SEGTREE_LAZY_MAX
+const ll segtree_lazy_init = 0;
+inline ll segtree_lazy_merge(ll a, ll b) {// 演算子aとbのマージ
+	return max(a, b);
+}
+inline ll segtree_lazy_multi(ll a, ll n) {// 演算子aをn個つなげたもの
+	return a;
+}
+inline ll segtree_lazy_divide(ll a) {// 演算子aを二個に分ける
+	return a;
+}
+inline ll segtree_lazy_apply(ll a, ll x) {// aに演算子xを適用する
+	return max(a, x);
+}
+#endif
+#ifdef SEGTREE_LAZY_MIN
+const ll segtree_lazy_init = INF;
+inline ll segtree_lazy_merge(ll a, ll b) {// 演算子aとbのマージ
+	return min(a, b);
+}
+inline ll segtree_lazy_multi(ll a, ll n) {// 演算子aをn個つなげたもの
+	return a;
+}
+inline ll segtree_lazy_divide(ll a) {// 演算子aを二個に分ける
+	return a;
+}
+inline ll segtree_lazy_apply(ll a, ll x) {// aに演算子xを適用する
+	return min(a, x);
+}
+#endif
 
 class SegmentTree {
 public:
@@ -12,7 +100,7 @@ public:
 		N = 0;
 		node = NULL;
 	}
-	void activate(int N, ll value) {// 初期化
+	inline void activate(int N, ll value = segtree_data_init) {// 初期化
 		if (node != NULL) { delete[] node; }
 
 		this->N = N;
@@ -22,62 +110,54 @@ public:
 		for (int i = 0; i < 2 * size - 1; i++) { node[i] = value; }
 	}
 
-	void activate(vector<ll> v) {
+	inline void activate(vector<ll> v) {
 		if (node != NULL) { delete[] node; }
 
-		N = v.size();
+		N = (int)v.size();
 		size = 1; while (size < N) { size *= 2; }
 
 		node = new ll[2 * size - 1];
 
 		for (ll i = 0; i < N; i++) node[i + size - 1] = v[i];
-		for (ll i = size - 2; i >= 0; i--) node[i] = segtree_func(node[i * 2 + 1], node[i * 2 + 2]);
+		for (ll i = size - 2; i >= 0; i--) node[i] = segtree_data_merge(node[i * 2 + 1], node[i * 2 + 2]);
 	}
 
-	void update(int pos, ll v) {// posの値をvに更新する
+	inline void set(int pos, ll v) {// posの値をvに更新する
 		int k = pos + size - 1;
 		node[k] = v;
 		while (k > 0) {
 			k = (k - 1) / 2;
-			node[k] = segtree_func(node[k * 2 + 1], node[k * 2 + 2]);
+			node[k] = segtree_data_merge(node[k * 2 + 1], node[k * 2 + 2]);
 		}
 	}
 
-	ll get(ll pos) {// posの値を取得する
+	inline ll get(ll pos) {// posの値を取得する
 		if (pos < 0 || pos >= N) {
-			cout << "get : pos = "<< pos << "は範囲[0, " << N << ")を超えています" << endl;
 			return 0;
 		}
 		ll k = pos + size - 1;
 		return node[k];
 	}
-	ll get(ll L, ll R) {// 半開区間 [L, R) における最小値/最大値取得などに答える
+	inline ll get(ll L, ll R) {// 半開区間 [L, R) における最小値/最大値取得などに答える
 		if (L > R) {
 			ll s = L; L = R; R = s;
-			cout << "get : LとRの大小関係が逆です" << endl;
 		}
-		if (L < 0) {
-			L = 0;
-			cout << "get : Lが0未満の値でした" << endl;
-		}
-		if (R > N) {
-			R = N;
-			cout << "get : RがNよりも大きな値でした" << endl;
-		}
+		L = max(L, (ll)0);
+		R = min(R, (ll)N);
+
 		if (L == R) {
-			cout << "get : 区間の幅が0でした" << endl;
 			return 0;
 		}
 		return query(L, R, 0, 0, size);
 	}
 
-	void DebugCout() {// デバッグ用
+	inline void DebugCout() {// デバッグ用
 		cout << endl << endl;
 		cout << "範囲 : " << 0 << " to " << N - 1 << endl;
 		int k = 2;
 		for (int i = 0; i < size * 2 - 1; i++) {
-			if (node[i] == INF) {
-				cout << "INF ";
+			if (node[i] == segtree_data_init) {
+				cout << "INIT ";
 			}
 			else {
 				cout << node[i] << " ";
@@ -101,8 +181,88 @@ private:
 		if (c <= a) { return query(a, b, k * 2 + 2, c, r); }
 		if (b <= c) { return query(a, b, k * 2 + 1, l, c); }
 
-		return segtree_func(query(a, b, k * 2 + 2, c, r), query(a, b, k * 2 + 1, l, c));
+		return segtree_data_merge(query(a, b, k * 2 + 2, c, r), query(a, b, k * 2 + 1, l, c));
 	}
+};
+
+///////////////   遅延伝播セグメント木
+class LazySegmentTree {
+public:
+	LazySegmentTree(vector<ll> v) {
+		N = (int)v.size();
+		size = 1; while (size < N) size *= 2;
+		node.resize(2 * size - 1, segtree_data_init);
+		lazy.resize(2 * size - 1, 0);
+
+		for (int i = 0; i < N; i++) node[i + size - 1] = v[i];
+		for (int i = size - 2; i >= 0; i--) node[i] = segtree_data_merge(node[i * 2 + 1], node[i * 2 + 2]);
+	}
+	LazySegmentTree(int N0, ll init) {
+		N = N0;
+		size = 1; while (size < N) size *= 2;
+		node.resize(2 * size - 1, segtree_data_init);
+		lazy.resize(2 * size - 1, 0);
+
+		for (int i = 0; i < N; i++) node[i + size - 1] = init;
+		for (int i = size - 2; i >= 0; i--) node[i] = segtree_data_merge(node[i * 2 + 1], node[i * 2 + 2]);
+	}
+
+
+	inline void set(int a, int b, ll x) {// [a, b) に演算子xを設定する
+		set(a, b, x, 0, 0, size);
+	}
+	inline ll get(int a, int b) {// [a, b) の要素の合計を得る
+		return get(a, b, 0, 0, size);
+	}
+
+	void DebugCout() {// デバッグ用
+		cout << endl << endl;
+		cout << "範囲 : " << 0 << " to " << N - 1 << endl;
+		int k = 2;
+		for (int i = 0; i < size * 2 - 1; i++) {
+			cout << node[i] << "(" << lazy[i] << ") ";
+			if (i + 2 == k) {
+				cout << endl;
+				k *= 2;
+			}
+		}
+		cout << endl << endl;
+	}
+private:
+	inline void eval(int k, int l, int r) {
+		if (lazy[k] != 0) {
+			node[k] = segtree_lazy_apply(node[k], lazy[k]);
+			if (r - l > 1) {
+				lazy[2 * k + 1] = segtree_lazy_merge(lazy[2 * k + 1], segtree_lazy_divide(lazy[k]));
+				lazy[2 * k + 2] = segtree_lazy_merge(lazy[2 * k + 2], segtree_lazy_divide(lazy[k]));
+			}
+			lazy[k] = segtree_lazy_init;
+		}
+	}
+	void set(int a, int b, ll x, int k, int l, int r) {
+		eval(k, l, r);
+		if (b <= l || r <= a) return;
+		if (a <= l && r <= b) {
+			lazy[k] = segtree_lazy_merge(lazy[k], segtree_lazy_multi(x, r - l));
+			eval(k, l, r);
+		}
+		else {
+			set(a, b, x, 2 * k + 1, l, (l + r) / 2);
+			set(a, b, x, 2 * k + 2, (l + r) / 2, r);
+			node[k] = segtree_data_merge(node[2 * k + 1], node[2 * k + 2]);
+		}
+	}
+	ll get(int a, int b, int k, int l, int r) {
+		eval(k, l, r);
+		if (b <= l || r <= a) return 0;
+		if (a <= l && r <= b) return node[k];
+		ll vl = get(a, b, 2 * k + 1, l, (l + r) / 2);
+		ll vr = get(a, b, 2 * k + 2, (l + r) / 2, r);
+		return segtree_data_merge(vl, vr);
+	}
+
+	int N, size;
+	vector<ll> node, lazy;
 };
 
 ///////////////   BIT
@@ -146,7 +306,7 @@ public:
 		}
 		return s;
 	}
-	void add(int i, int x) {// i番目の要素にxを加える
+	void add(int i, ll x) {// i番目の要素にxを加える
 		if (i < 0 || i >= N) {
 			return;
 		}
@@ -159,7 +319,7 @@ public:
 	ll get(int i) {
 		return BIT_FUNCTION(sum(i), BIT_INVERSE(sum(i - 1)));
 	}
-	void set(int i, int x) {// i番目の要素をxに変更する
+	void set(int i, ll x) {// i番目の要素をxに変更する
 		ll diff = BIT_FUNCTION(x, BIT_INVERSE(get(i)));
 		add(i, diff);
 	}
@@ -170,22 +330,19 @@ public:
 
 int main() {
 	int N, M; cin >> N >> M;
-	SegmentTree ST; ST.activate(N, INF);
+	SegmentTree ST; ST.activate(N);
 
 	for (int i = 0; i < M; i++) {
 		int a, b; cin >> a >> b;
-		a--;
-		ST.update(a, b);
+		ST.set(a, b);
 	}
 
 	ST.DebugCout();
 
-	cout << ST.get(0, 3) << endl;
-	cout << ST.get(4, 6) << endl;
-	cout << ST.get(0, 5) << endl;
-	cout << ST.get(0, N) << endl;
-
-	cout << ST.get(9, -1) << endl;// 不正な入力
+	cout << "[0, 3) : " << ST.get(0, 3) << endl;
+	cout << "[4, 6) : " << ST.get(4, 6) << endl;
+	cout << "[0, 5) : " << ST.get(0, 5) << endl;
+	cout << "[0, 8) : " << ST.get(0, 8) << endl;
 
 	return 0;
 }
