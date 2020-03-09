552c21423d1240b3e02425413efe3316d9055b05
SoshunNaito <50867811+SoshunNaito@users.noreply.github.com>
2020-03-09 22:21:57 +0900

    template.cppの改良
    unordered_setとunordered_mapを加えました

--- template.cpp
+++ template.cpp
@@ -12,7 +12,9 @@
 #include <deque>
 #include <list>
 #include <set>
+#include <unordered_set>
 #include <map>
+#include <unordered_map>
 #include <bitset>
 #include <random>
 #include <complex>
@@ -22,13 +24,13 @@ using namespace std;
 typedef long long ll;
 
 template<typename T>
-inline bool chmin(T& x, T& a) {
+inline bool chmin(T& x, T a) {
 	if (x >= a) { x = a; return true; }
 	return false;
 }
 
 template<typename T>
-inline bool chmax(T& x, T& a) {
+inline bool chmax(T& x, T a) {
 	if (x <= a) { x = a; return true; }
 	return false;
 }
